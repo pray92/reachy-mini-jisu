4e2a090ba094b39527b7dedd89345379cd8e51e9
Jisu Choi <redgem92@gmail.com>
2026-04-10 21:24:11 +0900

    FIX : [jisu-choi] Merge-conflicted

--- study/jisuchoi/second/rotate_head.py
+++
@@ -1,8 +0,0 @@
-from reachy_mini import ReachyMini, ReachyMiniApp
-from reachy_mini.utils import create_head_pose
-
-with ReachyMini() as mini:
-    # 머리를 위로 들고(z축) 롤(roll) 회전
-    pose = create_head_pose(z=10, roll=15, degrees=True, mm=True)
-    mini.goto_target(head=pose, duration=2.0)
-
